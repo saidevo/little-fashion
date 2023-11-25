b76a31cff3e33b08d6beb969fca8d79c400db8db
saidevo <122390185+saidevo@users.noreply.github.com>
2023-11-25 20:30:03 +0530

    Create 2127_little_fashion.sh

---
+++ 2127_little_fashion.sh
@@ -0,0 +1,4 @@
+sudo yum install httpd -y
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/little-fashion.zip
+unzip -o little-fashion.zip
+sudo mv 2127_little_fashion /var/www/html
